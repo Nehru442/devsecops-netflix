40d8864a6ca9388408d7bf6d48a3bf4ce3f8aba4
Nehru442 <nehru.k1411@gmail.com>
2026-07-13 15:52:14 +0530

    Change S3 bucket name in backend configuration

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-nehru-1" # Replace with your actual S3 bucket name
+    bucket = "devsecops-netflix-nehru-2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
